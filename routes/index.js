146572f47ab51ede8ac4f34204ff49600e5593e6
harshdeep-jft <harshdeep.saiyam@jellyfishtechnologies.com>
2020-09-03 14:41:58 +0530

    Add cool face API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const cool = require('cool-ascii-faces');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: cool() });
 });
+
+router.get('/cool',(req, res)=>{
+  res.send(cool());
+})
 
 module.exports = router;
